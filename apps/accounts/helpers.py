15911f068c0044156c9b077562e7e94b95b4ee7c
Mark Striemer <mstriemer@mozilla.com>
2015-11-27 17:03:10 -0600

    Set FxA state in body[data-fxa-config]

--- apps/accounts/helpers.py
+++ apps/accounts/helpers.py
@@ -1,13 +1,43 @@
+import json
+
 from django.conf import settings
 
+import jinja2
 from jingo import register
 
 
 @register.function
-def fxa_config():
-    return {camel_case(key): value
-            for key, value in settings.FXA_CONFIG.iteritems()
-            if key != 'client_secret'}
+@jinja2.contextfunction
+def fxa_config(context):
+    request = context['request']
+    config = {camel_case(key): value
+              for key, value in settings.FXA_CONFIG.iteritems()
+              if key != 'client_secret'}
+    set_fxa_state(request)
+    config['state'] = request.session['fxa_state']
+    return config
+
+
+def set_fxa_state(request):
+    if needs_fxa_state(request):
+        request.session['fxa_state'] = json.dumps(fxa_state(request.user))
+
+
+def needs_fxa_state(request):
+    def load_fxa_state():
+        return json.loads(request.session['fxa_state'])
+
+    try:
+        return ('fxa_state' not in request.session or
+                load_fxa_state()['user_id'] != request.user.pk)
+    except ValueError:
+        return True
+
+
+def fxa_state(user):
+    return {
+        'user_id': user.pk,
+    }
 
 
 def camel_case(snake):
